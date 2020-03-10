fd031a5282980010f85878ce5ef629987f8647ad
Pim <Pim.pallada@gmail.com>
2020-03-10 15:04:11 +0100

    Fixed indenting

--- resources/views/pages/city.blade.php
+++ resources/views/pages/city.blade.php
@@ -2,10 +2,11 @@
 
 @section('content')
     <h1>{{ $city }}</h1>
-@for($i = 1; $i <= count($answers); $i++)
-    
-    <p>vraag {{ $i }}: {{$answers["vraag$i"]}} </p>
 
-@endfor
+    @for($i = 1; $i <= count($answers); $i++)
+    
+        <p>vraag {{ $i }}: {{$answers["vraag$i"]}} </p>
+
+    @endfor
 
 @endsection
