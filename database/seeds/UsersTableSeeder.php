6697bd747f38e507d0ff81a1c1beeb39d8b89aca
Danny <d.berkelaar1@student.avans.nl>
2020-04-15 09:53:14 +0200

    users seed

---
+++ database/seeds/UsersTableSeeder.php
@@ -0,0 +1,22 @@
+<?php
+
+use Illuminate\Database\Seeder;
+use Illuminate\Support\Facades\Hash;
+use App\User;
+
+class UsersTableSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     *
+     * @return void
+     */
+    public function run()
+    {
+        User::create([
+            'name' => 'Test',
+            'email' => 'test@test.com',
+            'password' => Hash::make('test')
+        ]);
+    }
+}
