eb5458b516b5a51c536dab77f7d76828360aeae8
Thierry Rietveld <thierry.rietveld0505@gmail.com>
2020-04-16 10:17:36 +0200

    Manjofiek

--- resources/js/classes/Map.js
+++ resources/js/classes/Map.js
@@ -5,7 +5,7 @@ export default class Map {
         this.lat = lat;
         this.long = long;
         this.targetElement = targetElement
-		this.layers = [];
+        this.layers = [];
         this.map = this.createMap();
 
         this.inputs = document.querySelectorAll(".m-map--container__selections input");
@@ -17,19 +17,25 @@ export default class Map {
     setEventListeners() {
         this.inputs.forEach(i => {
             i.addEventListener('change', e => {
-                if(e.target.checked) {
-                    var checkboxes = document.getElementsByName(i.name);
-                for(var j=0; j < checkboxes.length; ++j) {
-                    checkboxes[j].checked = true;
-                        }
+
+                if (e.target.checked) {
+                    let checkboxes = document.getElementsByName(i.name);
+
+                    for (let j = 0; j < checkboxes.length; ++j) {
+                        checkboxes[j].checked = true;
+                    }
+
                     this.map.addLayer(this.findLayer(i.value).layer)
                 } else {
-                    var checkboxes = document.getElementsByName(i.name);
-                for(var j=0; j < checkboxes.length; ++j) {
-                    checkboxes[j].checked = false;
-                        }
+                    let checkboxes = document.getElementsByName(i.name);
+
+                    for (let j = 0; j < checkboxes.length; ++j) {
+                        checkboxes[j].checked = false;
+                    }
+
                     this.map.removeLayer(this.findLayer(i.value).layer)
                 }
+
             })
         })
     }
@@ -42,8 +48,8 @@ export default class Map {
 
     findLayer(layerName) {
 
-        for(let i = 0; i < this.layers.length; i++) {
-            if(this.layers[i].layerName == layerName) return this.layers[i];
+        for (let i = 0; i < this.layers.length; i++) {
+            if (this.layers[i].layerName == layerName) return this.layers[i];
         }
     }
 
@@ -54,16 +60,16 @@ export default class Map {
 
     createMap() {
         return new ol.Map({
-			target: this.targetElement,
-			layers: [
-				new ol.layer.Tile({
-					source: new ol.source.OSM()
-				})
-			],
-			view: new ol.View({
-				center: ol.proj.fromLonLat([this.long, this.lat]),
-				zoom: 10
-			})
-		});
+            target: this.targetElement,
+            layers: [
+                new ol.layer.Tile({
+                    source: new ol.source.OSM()
+                })
+            ],
+            view: new ol.View({
+                center: ol.proj.fromLonLat([this.long, this.lat]),
+                zoom: 10
+            })
+        });
     }
 }
