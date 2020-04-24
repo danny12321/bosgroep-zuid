bb18dfee6bc2cd8dbcd41241e8a4af21c6f23dc7
Thierry Rietveld <thierry.rietveld0505@gmail.com>
2020-04-24 12:44:38 +0200

    Filter municipalities on homepage

--- resources/js/app.js
+++ resources/js/app.js
@@ -1,1 +1,8 @@
 require('./bootstrap');
+import MunicipalitySearchAutocomplete from './classes/MunicipalitySearchAutocomplete';
+
+const municipalities = JSON.parse(document.querySelector(".m-php__municipalities").innerHTML);
+const searchInput = document.querySelector(".l-home__header__filter__search__input");
+const itemsElement = document.querySelector(".l-home__header__filter__search__items");
+
+new MunicipalitySearchAutocomplete(searchInput, itemsElement, municipalities);
