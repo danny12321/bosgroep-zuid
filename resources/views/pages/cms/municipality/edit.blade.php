f5014f81da8e78b89f81253d9fbdcd451bf6593c
Danny <d.berkelaar1@student.avans.nl>
2020-05-07 12:21:42 +0200

    added default zoom to municipality and changed de form look.

--- resources/views/pages/cms/municipality/edit.blade.php
+++ resources/views/pages/cms/municipality/edit.blade.php
@@ -1,9 +1,13 @@
 @extends('layouts.cms')
 
+@section('head')
+    @include('inc.openlayers')
+@endsection
+
 @section('content')
     <h1>Gemeente wijzigen</h1>
 
-    <form method="post" action="{{ route('cms_municipality_update', ['municipality' => $municipality->id]) }}">
+    <form id="municipality-form" method="post" action="{{ route('cms_municipality_update', ['municipality' => $municipality->id]) }}">
         @csrf
         @method('PUT')
 
@@ -31,26 +35,33 @@
         </div>
 
         <div class="form-group">
-            <label for="lat">Breedtegraad (Lat)</label>
-            <input class="form-control @error('lat') is-invalid @enderror" value="{{ old('lat', $municipality->lat) }}" type="number" step="any" name="lat" id="lat">
+            <label>Standaard kaart weergaven</label>
+            <div id="map"
+                style="height: 500px"
+            ></div>
 
             @error('lat') 
                 <div class="invalid-feedback">
                     {{ $errors->first("lat") }}
                 </div>
             @enderror
-        </div>
-
-        <div class="form-group">
-            <label for="long">Lengtegraad (Long)</label>
-            <input class="form-control @error('long') is-invalid @enderror" value="{{ old('long', $municipality->long) }}" type="number" step="any" name="long" id="long">
 
             @error('long') 
                 <div class="invalid-feedback">
                     {{ $errors->first("long") }}
                 </div>
             @enderror
+
+            @error('zoom') 
+                <div class="invalid-feedback">
+                    {{ $errors->first("zoom") }}
+                </div>
+            @enderror
         </div>
+
+        <input class="form-control d-none @error('lat') is-invalid @enderror" value="{{ old('lat') }}" type="number" step="any" name="lat" id="lat">
+        <input class="form-control d-none @error('long') is-invalid @enderror" value="{{ old('long') }}" type="number" step="any" name="long" id="long">
+        <input class="form-control d-none @error('zoom') is-invalid @enderror" value="{{ old('zoom') }}" type="number" step="any" name="zoom" id="zoom">
 
         <div class="m-cms__content__action--buttons">
             <button type="submit" class="btn btn-primary">Opslaan</button>
@@ -61,6 +72,7 @@
     <form action="{{ route('cms_municipality_destroy', ['municipality' => $municipality->id]) }}" id="delete-form" method="post">
         @csrf
         @method('DELETE')
-
     </form>
+
+    <script src="{{ asset('js/municipalityMap.js') }}" defer></script>
 @endsection
--- webpack.mix.js
+++ webpack.mix.js
@@ -16,4 +16,5 @@ mix.js('resources/js/app.js', 'public/js')
     .js('resources/js/home.js', 'public/js') 
     .js('resources/js/questions.js', 'public/js') 
     .js('resources/js/answerQuestion.js', 'public/js') 
+    .js('resources/js/municipalityMap.js', 'public/js') 
     .sass('resources/sass/app.scss', 'public/css');
