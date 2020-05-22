7b709b62db2ab337048196da73f0615b9b810d7c
Danny <d.berkelaar1@student.avans.nl>
2020-05-22 21:01:08 +0200

    Added the legend

--- resources/views/pages/map.blade.php
+++ resources/views/pages/map.blade.php
@@ -8,8 +8,8 @@
     <script src="https://cdn.jsdelivr.net/gh/openlayers/openlayers.github.io@master/en/v6.2.1/build/ol.js"></script>
 </head>
 <body>
-
     <div class="m-map--container">
+        <img src="http://gmd.has.nl:8080/geoserver/biodiversiteithorst/wms?REQUEST=GetLegendGraphic&VERSION=1.0.0&FORMAT=image/png&WIDTH=20&HEIGHT=20&STRICT=false&style=biodiversiteithorst:StijlStressfactoren&legend_options=dx:5;&TRANSPARENT=true" alt="Legenda" class="m-map--container__legend">
         <div id="map" class="m-map--container__map"></div>
         <div class="m-map--container__selections">
             @include('modules.selectionlist', ['selections' => $selections, 'filters' => $filters])
