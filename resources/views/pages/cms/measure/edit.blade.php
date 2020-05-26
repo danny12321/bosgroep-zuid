0c33ab98b6fcc6158ce9b209aba5fc0f88d8afb4
Thierry Rietveld <thierry.rietveld0505@gmail.com>
2020-05-26 17:16:49 +0200

    Veel merge meuk en added maatregel delete

--- resources/views/pages/cms/measure/edit.blade.php
+++ resources/views/pages/cms/measure/edit.blade.php
@@ -59,16 +59,28 @@
                     <input type="radio" name="problem_id" id="problem-{{$problem->id}}" value="{{$problem->id}}" @if($problem->id == $measure->problem_id)) checked @endif>
                     <label for="problem-{{$problem->id}}">{{$problem->name}}</label>
                 </div>
+
+                <div class="form-group">
+                    <input type="radio" name="problem_id" id="problem-null" value="" @if($problem->id == $measure->problem_id)) checked @endif>
+                    <label for="problem-null">Geen</label>
+                </div>
             @endforeach
 
-            <div class="form-group">
-                <input type="radio" name="problem_id" id="problem-null" value="" @if($problem->id == $measure->problem_id)) checked @endif>
-                <label for="problem-null">Geen</label>
-            </div>
         </div>
 
 
         <input name="municipality_id" type="hidden" value="{{$measure->municipality_id}}">
-        <button type="submit" class="btn btn-primary">Opslaan</button>
+
+        <div class="m-cms__content__action--buttons">
+            <button type="submit" class="btn btn-primary">Opslaan</button>
+            <button class="btn btn-danger" type="submit" form="delete-form">Verwijder</button>
+        </div>
+
+    </form>
+
+    <form action="{{ route('cms_measure_destroy', ['measure' => $measure]) }}" id="delete-form" method="post">
+        @csrf
+        @method('DELETE')
+
     </form>
 @endsection
--- resources/views/pages/cms/municipality/municipality.blade.php
+++ resources/views/pages/cms/municipality/municipality.blade.php
@@ -15,69 +15,6 @@
                     <h2>Gidssoorten</h2>
                     <a href="{{ route('cms_guidespecies_create', ['municipality' => $municipality->id]) }}" class="btn btn-primary"><i class="fas fa-plus"></i></a>
                 </div>
-
-MAATREGELEN
-        {{-- <form action="{{ route('cms_guidespecies_destroy', ['municipality' => $municipality->id, 'guideSpecie' => $guidespecie->id]) }}" method="post">
-            @csrf
-            @method('DELETE')
-
-            <button class="btn btn-danger" type="submit">Delete</button>
-        </form>
-    </div>
-    @endforeach
-
-    <h2>Opgaven</h2>
-    <a href="{{ route('cms_problem_create', ['municipality' => $municipality->id]) }}">Opgave toevoegen</a>
-    
-    @foreach ($problems as $problem)
-    <div>
-        {{$problem->name}}
-        
-        <a href="{{ route('cms_problem_edit', ['problem' => $problem->id]) }}">Wijzig</a>
-        
-        <form action="{{ route('cms_problem_destroy', ['municipality' => $municipality->id, 'problem' => $problem->id]) }}" method="post">
-            @csrf
-            @method('DELETE')
-
-            <button class="btn btn-danger" type="submit">Delete</button>
-        </form>
-    </div>
-    @endforeach
-
-    <h2>Lagen</h2>
-    <a href="{{ route('cms_layers_create', ['municipality' => $municipality->id]) }}">Laag toevoegen</a>
-
-
-    <table class="m-table">
-        <thead>
-            <th>Laag titel</th>
-            <th>Laag naam</th>
-            <th></th>
-        </thead>
-        <tbody>
-            @foreach ($layers as $layer)
-                <tr>
-                    <td>{{$layer->title}}</td>
-                    <td>{{$layer->name}}</td>
-                    <td class="m-table__edit">
-                        <a href="{{ route('cms_layers_edit', ['municipality' => $municipality->id, 'layer' => $layer->id]) }}">Wijzig</a>
-                    </td>
-                </tr>
-            @endforeach
-        </tbody>
-    </table>
-
-    <h2>Vragenlijst</h2>
-    @foreach ($questions as  $indexKey => $question)
-    <div>
-            
-        <h3>
-            {{$indexKey + 1}}. {{$question->question}} 
-            <a class="btn" href="{{ route('cms_questions_edit', ['municipality' => $municipality->id, 'question' => $question->id]) }}"><i class="fas fa-edit"></i></a> 
-            <button class="btn btn-danger" form="delete-question-{{$question->id}}" type="submit"><i class="fas fa-trash-alt"></i></button>
-    </h3> --}}
-END MAATREGELEN
-STYLING
                 <table class="m-table m-municipality__table">
                     <thead>
                         <th>Naam</th>
@@ -100,13 +37,42 @@ STYLING
                     </tbody>
                 </table>
             </div>
+
+            <div class="m-municipality__grid__grid--item m-card">
+                <div class="m-municipality__grid__grid--item__header">
+                    <h2>Opgaven</h2>
+                    <a href="{{ route('cms_problem_create', ['municipality' => $municipality->id]) }}" class="btn btn-primary"><i class="fas fa-plus"></i></a>
+                </div>
+
+                <table class="m-table m-municipality__table">
+                    <thead>
+                        <th>Naam</th>
+                        <th></th>
+                    </thead>
+                    <tbody>
+                        @if(!count($problems))
+                            <tr class="last-hover">
+                                <td><i>Geen gevonden</i></td>
+                            </tr>
+                        @endif
+                        @foreach ($problems as $problem)
+                            <tr class="hover last-hover">
+                                <td>{{ $problem->name }}}</td>
+                                <td class="m-table__edit">
+                                    <a href="{{ route('cms_problem_edit', ['problem' => $problem->id]) }}"><i class="fas fa-edit"></i></a>
+                                </td>
+                            </tr>
+                        @endforeach
+                    </tbody>
+                </table>
+            
+            </div>
     
             <div class="m-municipality__grid__grid--item m-card">
                 <div class="m-municipality__grid__grid--item__header">
                     <h2>GeoServer Lagen</h2>
                     <a href="{{ route('cms_layers_create', ['municipality' => $municipality->id]) }}" class="btn btn-primary"><i class="fas fa-plus"></i></a>
                 </div>
-END STYLING
         
                 <table class="m-table m-municipality__table">
                     <thead>
@@ -152,7 +118,7 @@ END STYLING
                             <tr class="clickable hover last-hover" onclick="document.location = '{{ route('cms_questions_show', ['municipality' => $municipality->id, 'question' => $question->id]) }}';">
                                 <td>{{$question->question}}</td>
                                 <td class="m-table__edit">
-                                    {{-- If the a tag is not here, the transistion of the tr does not work. No idea why ¯\(°_o)/¯ --}}
+                                    {{-- If this <a> tag is not here, the transistion of the tr does not work. No idea why ¯\(°_o)/¯ --}}
                                     <a style="opacity: 0;"></a>
                                 </td>
                             </tr>
@@ -161,18 +127,7 @@ END STYLING
                 </table>
         
             </div>
-    
-MAATREGELEN
-    {{-- @foreach ($measures as $measure)
-    <div>
-        {{$measure->name}}
-        
-        <a href="{{ route('cms_measure_edit', ['measure' => $measure->id , 'municipality' => $municipality->id]) }}">Wijzig</a>
-        
-        <form action="{{ route('cms_measure_destroy', ['measure' => $measure->id]) }}" method="post">
-            @csrf
-            @method('DELETE') --}}
-STYLING
+
             <div class="m-municipality__grid__grid--item m-card">
                 <div class="m-municipality__grid__grid--item__header">
                     <h2>Maatregelen</h2>
@@ -194,7 +149,7 @@ STYLING
                             <tr class="hover last-hover">
                                 <td>{{$measure->name}}</td>
                                 <td class="m-table__edit">
-                                    <a style="opacity:0;"></a>
+                                    <a href="{{ route('cms_measure_edit', ['measure' => $measure->id , 'municipality' => $municipality->id]) }}"><i class="fas fa-edit"></i></a>
                                 </td>
                             </tr>
                         @endforeach
@@ -202,6 +157,5 @@ STYLING
                 </table>
             </div>
         </div>
-END STYLING
-</div>
+    </div>
 @endsection
