122d6d35f0b94f8ae292dd6604b1107fa219e020
Danny <d.berkelaar1@student.avans.nl>
2020-05-27 14:40:00 +0200

    Only show remaining layers if there are any 😏

--- resources/views/modules/selectionlist.blade.php
+++ resources/views/modules/selectionlist.blade.php
@@ -14,17 +14,18 @@
     </div>
 @endforeach
 
+@if (count($municipality->layers_without_guidespecie))
+    <div href="#SubMenu-overig" class="list-group-item list-group-item-primary collapsed" data-toggle="collapse" data-parent="#SubMenu-overig">
+        <span>Overig<span>
+        <i class="fa fa-caret-down"></i>
+    </div>
 
-<div href="#SubMenu-overig" class="list-group-item list-group-item-primary collapsed" data-toggle="collapse" data-parent="#SubMenu-overig">
-    <span>Overig<span>
-    <i class="fa fa-caret-down"></i>
-</div>
-
-<div class="collapse list-group-submenu" id="SubMenu-overig">
-    @foreach ($municipality->layers_without_guidespecie as $layer)
-        <div class="list-group-item">
-            <input type="checkbox" id="{{$layer->name}}" name="{{$layer->name}}" value="{{$layer->name}}"{{ in_array($layer->id, $filters) ? 'checked' : ''}}>
-            <label for="{{$layer->name}}">{{$layer->title}}</label>
-        </div>
-    @endforeach
-</div>
+    <div class="collapse list-group-submenu" id="SubMenu-overig">
+        @foreach ($municipality->layers_without_guidespecie as $layer)
+            <div class="list-group-item">
+                <input type="checkbox" id="{{$layer->name}}" name="{{$layer->name}}" value="{{$layer->name}}"{{ in_array($layer->id, $filters) ? 'checked' : ''}}>
+                <label for="{{$layer->name}}">{{$layer->title}}</label>
+            </div>
+        @endforeach
+    </div>
+@endif
